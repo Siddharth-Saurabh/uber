d486d87377abde536c8833e9232cbfb80ea243d6
Siddharth-Saurabh <jhasiddharth9709@gmail.com>
2025-03-16 18:40:32 +0530

    captain added

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,34 @@
-const dotenv=require('dotenv');
+const dotenv = require('dotenv');
 dotenv.config();
-
 const express = require('express');
-const app=express();
-const cors=require('cors');
-const connectToDb=require('./db/db');
-app.use(cors());
-const  userRoutes=require('./routes/userRoutes');
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-app.use('/api/users',userRoutes);
+const cors = require('cors');
+const app = express();
+const cookieParser = require('cookie-parser');
+const connectToDb = require('./db/db');
+const userRoutes = require('./routes/user.routes');
+const captainRoutes = require('./routes/captain.routes');
+const mapsRoutes = require('./routes/maps.routes');
+const rideRoutes = require('./routes/ride.routes');
 
 connectToDb();
 
-app.get('/',(req,res)=>{
-    res.send('hello');
-})
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-module.exports=app;
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.use('/users', userRoutes);
+app.use('/captains', captainRoutes);
+
+
+
+
+
+module.exports = app;
+
--- backend/db/db.js
+++ backend/db/db.js
@@ -1,9 +1,12 @@
-const mongoose = require('mongoose');  // Corrected
+const mongoose = require("mongoose");
 
 function connectToDb() {
-    mongoose.connect(process.env.DB_CONNECT)
-        .then(() => console.log('Connected to DB'))  // Use .then() for success message
-        .catch(err => console.log('DB Connection Error:', err));  // Catch connection errors
+  mongoose
+    .connect(process.env.DB_CONNECT)
+    .then(() => {
+      console.log("Connected to DB");
+    })
+    .catch((err) => console.log(err));
 }
 
 module.exports = connectToDb;
